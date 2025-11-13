af0a28de761bcd54ba0d251e56f515cfb2eaae8a
amineelakkermi <amineelakkermi37@gmail.com>
2025-11-14 00:37:10 +0100

    mise a jour

--- app/dashboard/edit/[id]/page.tsx
+++ app/dashboard/edit/[id]/page.tsx
@@ -1,13 +1,13 @@
-// app/dashboard/edit/[id]/page.tsx
 'use client'
+
+import React from 'react'
 import UpdateProject from '../../UpdateProject'
 
-
-export default async function EditProjectByIdPage({ params }: { params: { id: string } }) {
-  const { id } = await params
+export default function EditProjectByIdPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = React.use(params)
 
   return (
-      <section className="relative  isolate min-h-screen flex flex-col items-center justify-center bg-white px-4 md:px-10">
+    <section className="relative isolate min-h-screen flex flex-col items-center justify-center bg-white px-4 md:px-10">
       <div className="z-10 w-full max-w-4xl p-6 rounded-2xl shadow-lg bg-white/90 backdrop-blur-md">
         <UpdateProject id={id} />
       </div>
